9a4fb1719aa906b761e2fe546d21b8ef32dfc048
papanito <papanito@wyssmann.com>
2023-08-29 09:46:23 +0200

    feat: enable firejail, tor service

--- networking.nix
+++ networking.nix
@@ -23,16 +23,87 @@
     ''
       10.0.0.10 yuno.home
     '';
-  # Open ports in the firewall.
-  networking.firewall.allowedTCPPortRanges = [
-  # KDE Connect
-    { from = 1714; to = 1764; }
-  ];
-  networking.firewall.allowedUDPPortRanges = [
-    # KDE Connect
-    { from = 1714; to = 1764; }
-  ];
 
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  programs.firejail = {
+    enable = true;
+    wrappedBinaries = {
+      signal-desktop = {
+        executable = "${pkgs.signal-desktop}/bin/signal-desktop --enable-features=UseOzonePlatform --ozone-platform=wayland";
+        profile = "${pkgs.firejail}/etc/firejail/signal-desktop.profile";
+        extraArgs = [ "--env=LC_ALL=C" "--env=GTK_THEME=Adwaita:dark" ];
+      };
+    };
+  };
+
+  services.tor = {
+    enable = true;
+    openFirewall = true;
+    settings = {
+      TransPort = [ 9040 ];
+      DNSPort = 5353;
+      VirtualAddrNetworkIPv4 = "172.30.0.0/16";
+    };
+  };
+
+  networking = {
+    # useNetworkd = true;
+    firewall = {
+      enable = true;
+      allowedTCPPortRanges = [
+        # KDE Connect
+        { from = 1714; to = 1764; }
+      ];
+      allowedUDPPortRanges = [
+        # KDE Connect
+        { from = 1714; to = 1764; }
+      ];
+
+      # interfaces.tornet = {
+      #   allowedTCPPorts = [ 9040 ];
+      #   allowedUDPPorts = [ 5353 ];
+      # };
+    };
+
+    # bridges."tornet".interfaces = [];
+    # nftables = {
+    #   enable = true;
+    #   ruleset = ''
+    #     table ip nat {
+    #       chain PREROUTING {
+    #         type nat hook prerouting priority dstnat; policy accept;
+    #         iifname "tornet" meta l4proto tcp dnat to 127.0.0.1:9040
+    #         iifname "tornet" udp dport 53 dnat to 127.0.0.1:5353
+    #       }
+    #     }
+    #   '';
+    # };
+    # nat = {
+    #   internalInterfaces = [ "tornet " ];
+    #   forwardPorts = [
+    #     {
+    #       destination = "127.0.0.1:5353";
+    #       proto = "udp";
+    #       sourcePort = 53;
+    #     }
+    #   ];
+    # };
+  };
+
+  # systemd.network = {
+  #   enable = false;
+  #   networks.tornet = {
+  #     matchConfig.Name = "tornet";
+  #     DHCP = "no";
+  #     networkConfig = {
+  #       ConfigureWithoutCarrier = true;
+  #       Address = "10.100.100.1/24";
+  #     };
+  #     linkConfig.ActivationPolicy = "always-up";
+  #   };
+  # };
+
+  # boot.kernel.sysctl = {
+  #   "net.ipv4.conf.tornet.route_localnet" = 1;
+  # };
+
 }
