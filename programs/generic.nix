93798fe35e03c52b5bc13f48eb3cb9960c7d71e8
papanito <papanito@wyssmann.com>
2023-08-29 07:17:08 +0200

    feat: add dnsutils and nix-direnv

--- programs/generic.nix
+++ programs/generic.nix
@@ -25,7 +25,7 @@
     spotify
     irssi
     helix
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    vim
   ];
   programs.gpaste.enable = true;
 }
