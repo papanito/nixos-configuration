b80fce8d74fdb3ab8ababaff10be55c5eb7f6803
papanito <papanito@wyssmann.com>
2023-08-29 09:45:41 +0200

    feat: add plymouth, reduce swapiness

--- boot.nix
+++ boot.nix
@@ -2,14 +2,21 @@
 
 {
  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  hardware.tuxedo-keyboard.enable = true;
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
+
+  hardware = {
+    tuxedo-keyboard.enable = true;
+  };
+
   boot.kernelParams = [
     "tuxedo_keyboard.mode=0"
     "tuxedo_keyboard.brightness=25"
     "tuxedo_keyboard.color_left=0x0000ff"
   ];
+
   # Setup keyfile
   boot.initrd.secrets = {
     "/crypto_keyfile.bin" = null;
@@ -18,5 +25,13 @@
   # Enable swap on luks
   boot.initrd.luks.devices."luks-7f34b0ef-aa8a-412a-933c-a42414371fad".device = "/dev/disk/by-uuid/7f34b0ef-aa8a-412a-933c-a42414371fad";
   boot.initrd.luks.devices."luks-7f34b0ef-aa8a-412a-933c-a42414371fad".keyFile = "/crypto_keyfile.bin";
+
+  boot.kernel.sysctl = {
+    "vm.swappiness" = 10;
+  };
+
+ environment.systemPackages = with pkgs; [
+    plymouth
+ ];
 }
 
