1c020802282a37f1927805801cfee647fe4a7937
papanito <papanito@wyssmann.com>
2023-08-31 16:43:39 +0200

    feat: move tuxedo config

--- programs/tuxedo.nix
+++ programs/tuxedo.nix
@@ -7,5 +7,11 @@ in {
     tuxedo.module
   ];
   hardware.tuxedo-control-center.enable = true;
-}
+  hardware.tuxedo-keyboard.enable = true;
 
+  boot.kernelParams = [
+    "tuxedo_keyboard.mode=0"
+    "tuxedo_keyboard.brightness=25"
+    "tuxedo_keyboard.color_left=0x0000ff"
+  ];
+}
