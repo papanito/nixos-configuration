dcb750f86633a4d569d81bc3460670d8ac5ca12d
papanito <papanito@wyssmann.com>
2023-08-31 16:43:39 +0200

    feat: move tuxedo config

--- boot.nix
+++ boot.nix
@@ -1,22 +1,6 @@
 { config, pkgs, ... }:
 
 {
- # Bootloader.
-  boot.loader = {
-    systemd-boot.enable = true;
-    efi.canTouchEfiVariables = true;
-  };
-
-  hardware = {
-    tuxedo-keyboard.enable = true;
-  };
-
-  boot.kernelParams = [
-    "tuxedo_keyboard.mode=0"
-    "tuxedo_keyboard.brightness=25"
-    "tuxedo_keyboard.color_left=0x0000ff"
-  ];
-
   # Setup keyfile
   boot.initrd.secrets = {
     "/crypto_keyfile.bin" = null;
@@ -25,13 +9,20 @@
   # Enable swap on luks
   boot.initrd.luks.devices."luks-7f34b0ef-aa8a-412a-933c-a42414371fad".device = "/dev/disk/by-uuid/7f34b0ef-aa8a-412a-933c-a42414371fad";
   boot.initrd.luks.devices."luks-7f34b0ef-aa8a-412a-933c-a42414371fad".keyFile = "/crypto_keyfile.bin";
-
-  boot.kernel.sysctl = {
-    "vm.swappiness" = 10;
+ 
+  # Bootloader and stuff
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+    plymouth = {
+      enable = true;
+      #themePackages = [ nixos-boot ];
+      #theme = "load_unload";
+    };
+    kernel.sysctl = {
+      "vm.swappiness" = 10;
+    };
   };
-
- environment.systemPackages = with pkgs; [
-    plymouth
- ];
 }
-
