3814016fcc06d87ff043fadf6be1b7143df44ea6
papanito <papanito@wyssmann.com>
2023-09-01 16:54:04 +0200

    feat: split programs by adding shell.nix

--- programs/generic.nix
+++ programs/generic.nix
@@ -4,25 +4,14 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    thefuck
-    gnupg
-    tmux
-    ghostscript
-    solo2-cli
-    mutt
-    irssi
-    helix
-    vim
     emacs
-    wl-clipboard
-    zellij
-    pueue
+    helix
+    irssi
+    mutt
     rpi-imager
-    gum # a tool for glamorous shell scripts
-    lsix # ls for images
-    vte # a library implementing a terminal emulator widget for GTK
-    fd # A simple, fast and user-friendly alternative to find
-    melt # Backup and restore Ed25519 SSH keys with seed words
+    solo2-cli
+    wl-clipboard
+    vim
   ];
   programs.gpaste.enable = true;
 }
