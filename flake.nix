7b5cb3731c56275c9c252fe2cf753a01085cb9c5
papanito <papanito@wyssmann.com>
2023-09-11 13:02:25 +0200

    feat: move tuxedo stuff to flake

--- flake.nix
+++ flake.nix
@@ -1,5 +1,6 @@
 {
   inputs = {
+    agenix.url = "github:ryantm/agenix";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     tuxedo-nixos = {
       url = "github:blitz/tuxedo-nixos";
@@ -7,11 +8,13 @@
     };
   };
 
-  outputs = { self, nixpkgs, tuxedo-nixos }: {
+  outputs = { self, nixpkgs, tuxedo-nixos, agenix }: {
     nixosConfigurations = {
       clawfinger = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
         modules = [
           ./configuration.nix
+          agenix.nixosModules.default
           tuxedo-nixos.nixosModules.default
           { hardware.tuxedo-control-center.enable = true; }
         ];
