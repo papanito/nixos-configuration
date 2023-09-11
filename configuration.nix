115a7719d294e297ee3ec1ba0203f948a23d5a6e
papanito <papanito@wyssmann.com>
2023-09-11 13:02:25 +0200

    feat: move tuxedo stuff to flake

--- configuration.nix
+++ configuration.nix
@@ -15,11 +15,7 @@
     ./fonts.nix
     ./printing.nix
     ./users.nix
-
-     "${builtins.fetchTarball "https://github.com/ryantm/agenix/archive/main.tar.gz"}/modules/age.nix"
   ];
-
-  environment.systemPackages = [ (pkgs.callPackage "${builtins.fetchTarball "https://github.com/ryantm/agenix/archive/main.tar.gz"}/pkgs/agenix.nix" {}) ];
 
   # Set your time zone.
   time.timeZone = "Europe/Zurich";
