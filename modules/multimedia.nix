590a93a880b898b729b10cf7797b6d4256a91512
papanito <papanito@wyssmann.com>
2023-09-23 17:32:09 +0200

    feat: add more apps

--- modules/multimedia.nix
+++ modules/multimedia.nix
@@ -5,6 +5,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     ghostscript
+    mpv # General-purpose media player, fork of MPlayer and mplayer2
     ncspot # Cross-platform ncurses Spotify client written in Rust, inspired by ncmpc and the likes
     spotify
     spotify-tui # Spotify for the terminal written in Rust
@@ -12,7 +13,9 @@
     yt-dlp
     ytmdl
     ffmpeg-full
+    gst_all_1.gstreamer # Open source multimedia framework
     peertube # A free software to take back control of your videos
+    streamlink # CLI for extracting streams from various websites to video player of your choosing
     vlc # Cross-platform media player and streaming server
     rhythmbox # A music playing application for GNOME
   ];
