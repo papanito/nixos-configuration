9f2069a4bd16787f3a0569bff8767ca3914abc9d
papanito <papanito@wyssmann.com>
2023-09-30 11:25:01 +0200

    feat: update system.autoUpgrade

--- configuration.nix
+++ configuration.nix
@@ -78,8 +78,18 @@
     };
   };
 
+  # https://discourse.nixos.org/t/best-practices-for-auto-upgrades-of-flake-enabled-nixos-systems/31255
   system.autoUpgrade = {
     enable = true;
+    flake = inputs.self.outPath;
+    flags = [
+      "--update-input"
+      "nixpkgs"
+      "--no-write-lock-file"
+      "-L" # print build logs
+    ];
+    dates = "02:00";
+    randomizedDelaySec = "45min";
   };
 
   console = {
