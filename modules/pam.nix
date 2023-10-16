b7568d7f222a729537e03f116e69df0fe47e2216
papanito <papanito@wyssmann.com>
2023-10-16 19:43:46 +0200

    feat: add udev rules for solo2

--- modules/pam.nix
+++ modules/pam.nix
@@ -1,8 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  services.udev.packages = [ pkgs.yubikey-personalization ];
-
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
@@ -14,14 +12,22 @@
   };
 
   # https://github.com/solokeys/solo2-cli/blob/main/70-solo2.rules
-  services.udev.extraRules = ''
-    # NXP LPC55 ROM bootloader (unmodified)
-    SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1fc9", ATTRS{idProduct}=="0021", TAG+="uaccess"
-    # NXP LPC55 ROM bootloader (with Solo 2 VID:PID)
-    SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1209", ATTRS{idProduct}=="b000", TAG+="uaccess"
-    # Solo 2
-    SUBSYSTEM=="tty", ATTRS{idVendor}=="1209", ATTRS{idProduct}=="beee", TAG+="uaccess"
-    # Solo 2
-    SUBSYSTEM=="usb", ATTRS{idVendor}=="1209", ATTRS{idProduct}=="beee", TAG+="uaccess"
-  '';
+  services.udev.packages = [
+    pkgs.yubikey-personalization
+    (pkgs.writeTextFile {
+      name = "wally_udev";
+      text = ''
+        # NXP LPC55 ROM bootloader (unmodified)
+        SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1fc9", ATTRS{idProduct}=="0021", TAG+="uaccess"
+        # NXP LPC55 ROM bootloader (with Solo 2 VID:PID)
+        SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1209", ATTRS{idProduct}=="b000", TAG+="uaccess"
+        # Solo 2
+        SUBSYSTEM=="tty", ATTRS{idVendor}=="1209", ATTRS{idProduct}=="beee", TAG+="uaccess"
+        # Solo 2
+        SUBSYSTEM=="usb", ATTRS{idVendor}=="1209", ATTRS{idProduct}=="beee", TAG+="uaccess"
+      '';
+      destination = "/etc/udev/rules.d/70-solo2.rules";
+    })
+  ];
+
 }
