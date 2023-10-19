35bef037b7c83c4bd4b44b3e11d4ca0728ffc132
papanito <papanito@wyssmann.com>
2023-10-19 11:16:05 +0200

    fix: hardware.tuxedo-control-center.enable

--- modules/tuxedo.nix
+++ modules/tuxedo.nix
@@ -1,13 +1,4 @@
 { config, pkgs, ... }:
+{
 
-let
-  tuxedo = import (builtins.fetchTarball "https://github.com/blitz/tuxedo-nixos/archive/master.tar.gz");
-in {
-  imports = [
-    tuxedo.module
-  ];
-
-  hardware = {
-    tuxedo-control-center.enable = true;
-  };
 }
