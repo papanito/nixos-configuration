ebdc38d0e67083afff332e4a6e9f7861edc8f46c
papanito <papanito@wyssmann.com>
2023-12-27 11:50:03 +0100

    feat: add restic backup to local disk

--- modules/services/restic.nix
+++ modules/services/restic.nix
@@ -14,10 +14,10 @@
 
   # security.wrappers.restic = {
   #   source = "${pkgs.restic.out}/bin/restic";
-  #   owner = "restic";
+  #   owner = "papanito";
   #   group = "users";
   #   permissions = "u=rwx,g=,o=";
-  #   capabilities = "cap_dac_read_search=+ep";
+  #   #capabilities = "cap_dac_read_search=+ep";
   # };
 
   age.secrets = {
@@ -29,89 +29,166 @@
     };
   };
 
-  services.restic.backups.b2 = {
-    initialize = false;
-    # since this uses an `agenix` secret that's only readable to the
-    # root user, we need to run this script as root. If your
-    # environment is configured differently, you may be able to do:
-    #
-    #user = "restic";
-    #
-    passwordFile = config.age.secrets.restic_password.path;
-    # what to backup.
-    paths = ["/home/papanito"];
-    # the name of your repository.
-    repository = "b2:papanito-private-backup:/clawfinger";
-    timerConfig = {
-      # backup every 1d
-      OnCalendar = "daily";
-      Persistent = true; 
+  services.restic.backups = {
+    b2 = {
+      initialize = false;
+      # since this uses an `agenix` secret that's only readable to the
+      # root user, we need to run this script as root. If your
+      # environment is configured differently, you may be able to do:
+      #
+      #user = "restic";
+      #
+      passwordFile = config.age.secrets.restic_password.path;
+      # what to backup.
+      paths = ["/home/papanito"];
+      # the name of your repository.
+      repository = "b2:papanito-private-backup:/clawfinger";
+      timerConfig = {
+        # backup every 1d
+        OnCalendar = "daily";
+        Persistent = true; 
+      };
+
+      environmentFile = config.age.secrets.restic_b2_credentials.path;
+
+      exclude = [
+        "**/cs"
+        "*/.android*"
+        "*/.Android*"
+        "*/.ansible/collections"
+        "*/.Apache*"
+        "*/.BurpSuite"
+        "*/.cache"
+        "*/.cargo"
+        "*/.chrome"
+        "*/.dartServer"
+        "*/.donet"
+        "*/.dotnet"
+        "*/.eclipse"
+        "*/.gem"
+        "*/.local"
+        "*/.m2/repository"
+        "*/.mozilla"
+        "*/.nuget"
+        "*/.nvm"
+        "*/.steam"
+        "*/.thumbnails"
+        "*/.vscode*/extensions"
+        "*/.wine"
+        "*/*.iso"
+        "*/*.log"
+        "*/*cache"
+        "*/*Cache"
+        "*/*thumbs"
+        "*/bin/*"
+        "*/*cache*"
+        "*/*Cache*"
+        "*/Downloads"
+        "*/Dropbox"
+        "*/Games"
+        "*/gnome-shell"
+        "*/go"
+        "*/google-chrome*"
+        "*/gPodder"
+        "*/Insync"
+        "*/Music"
+        "*/node_modules"
+        "*/Public"
+        "*/share"
+        "*/snap"
+        "**/tmp"
+        "*/Trash"
+        "*/Videos/Children"
+        "*/Videos/Movies"
+        "*/Videos/Music"
+        "*/Virtualbox"
+      ];
+
+      # keep 7 daily, 5 weekly, and 10 annual backups
+      pruneOpts = [
+        "--keep-daily 7"
+        "--keep-weekly 5"
+        "--keep-monthly 6"
+        "--keep-yearly 10"
+      ];
     };
+    local = {
+      initialize = true;
+      # since this uses an `agenix` secret that's only readable to the
+      # root user, we need to run this script as root. If your
+      # environment is configured differently, you may be able to do:
+      #
+      #user = "restic";
+      #
+      passwordFile = config.age.secrets.restic_password.path;
+      # what to backup.
+      paths = ["/home/papanito"];
+      # the name of your repository.
+      repository = "/run/media/papanito/Backup/backup/clawfinger";
+      timerConfig = {
+        # backup every 1d
+        OnCalendar = "daily";
+        Persistent = true; 
+      };
 
-    environmentFile = config.age.secrets.restic_b2_credentials.path;
+      exclude = [
+        "**/cs"
+        "*/.android*"
+        "*/.Android*"
+        "*/.ansible/collections"
+        "*/.Apache*"
+        "*/.BurpSuite"
+        "*/.cache"
+        "*/.cargo"
+        "*/.chrome"
+        "*/.dartServer"
+        "*/.donet"
+        "*/.dotnet"
+        "*/.eclipse"
+        "*/.gem"
+        "*/.local"
+        "*/.m2/repository"
+        "*/.mozilla"
+        "*/.nuget"
+        "*/.nvm"
+        "*/.steam"
+        "*/.thumbnails"
+        "*/.vscode*/extensions"
+        "*/.wine"
+        "*/*.iso"
+        "*/*.log"
+        "*/*cache"
+        "*/*Cache"
+        "*/*thumbs"
+        "*/bin/*"
+        "*/*cache*"
+        "*/*Cache*"
+        "*/Downloads"
+        "*/Dropbox"
+        "*/Games"
+        "*/gnome-shell"
+        "*/go"
+        "*/google-chrome*"
+        "*/gPodder"
+        "*/Insync"
+        "*/Music"
+        "*/node_modules"
+        "*/Public"
+        "*/share"
+        "*/snap"
+        "**/tmp"
+        "*/Trash"
+      ];
 
-    exclude = [
-      "**/cs"
-      "*/.android*"
-      "*/.Android*"
-      "*/.ansible/collections"
-      "*/.Apache*"
-      "*/.BurpSuite"
-      "*/.cache"
-      "*/.cargo"
-      "*/.chrome"
-      "*/.dartServer"
-      "*/.donet"
-      "*/.dotnet"
-      "*/.eclipse"
-      "*/.gem"
-      "*/.local"
-      "*/.m2/repository"
-      "*/.mozilla"
-      "*/.nuget"
-      "*/.nvm"
-      "*/.steam"
-      "*/.thumbnails"
-      "*/.vscode*/extensions"
-      "*/.wine"
-      "*/*.iso"
-      "*/*.log"
-      "*/*cache"
-      "*/*Cache"
-      "*/*thumbs"
-      "*/bin/*"
-      "*/*cache*"
-      "*/*Cache*"
-      "*/Downloads"
-      "*/Dropbox"
-      "*/Games"
-      "*/gnome-shell"
-      "*/go"
-      "*/google-chrome*"
-      "*/gPodder"
-      "*/Insync"
-      "*/Music"
-      "*/node_modules"
-      "*/Public"
-      "*/share"
-      "*/snap"
-      "**/tmp"
-      "*/Trash"
-      "*/Videos/Children"
-      "*/Videos/Movies"
-      "*/Videos/Music"
-      "*/Virtualbox"
-    ];
-
-    # keep 7 daily, 5 weekly, and 10 annual backups
-    pruneOpts = [
-      "--keep-daily 7"
-      "--keep-weekly 5"
-      "--keep-monthly 6"
-      "--keep-yearly 10"
-    ];
+      # keep 7 daily, 5 weekly, and 10 annual backups
+      pruneOpts = [
+        "--keep-daily 7"
+        "--keep-weekly 5"
+        "--keep-monthly 6"
+        "--keep-yearly 10"
+      ];
+    };
   };
-
   # Instead of doing this, you may alternatively hijack the
   # `awsS3Credentials` argument to pass along these environment
   # vars.
@@ -119,12 +196,10 @@
   # If you specified a user above, you need to change it to:
   # systemd.services.user.restic-backups-myaccount = { ... }
   #
-  systemd.services.restic-backups-b2 = {
-
-    # environment = {
-    #   B2_ACCOUNT_ID = "my_account_id_abc123";
-    #   B2_ACCOUNT_KEY = "my_account_key_def456";
-    # };
-  };
-
+  # systemd.services.restic-backups-b2 = {
+  #   # environment = {
+  #   #   B2_ACCOUNT_ID = "my_account_id_abc123";
+  #   #   B2_ACCOUNT_KEY = "my_account_key_def456";
+  #   # };
+  # };
 }
