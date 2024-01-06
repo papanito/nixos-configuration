7d03afc8a371e383404262e69e54e0f2696639ef
papanito <papanito@wyssmann.com>
2024-01-06 12:19:15 +0100

    feat: add options to pam module

--- hosts/clawfinger/default.nix
+++ hosts/clawfinger/default.nix
@@ -6,9 +6,19 @@
     ./hardware.nix
   ];
 
+  ## pam stuff
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  solokey.enable = true;
+
+  ## printing
   printing.enable = true;
   printing.drivers = [ pkgs.hplipWithPlugin ];
 
+  ## hardware specific system packages
   environment.systemPackages = [
     pkgs.linuxKernel.packages.linux_latest_libre.tuxedo-keyboard
   ];
--- modules/pam.nix
+++ modules/pam.nix
@@ -1,33 +1,37 @@
-{ config, pkgs, ... }:
+{ lib, config, pkgs, ... }:
 
+let
+   cfg = config.solokey;
+in
 {
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
+  options.solokey = {
+    enable 
+      = lib.mkEnableOption "enable pam using solokey";
   };
 
-  security.pam.services = {
-    login.u2fAuth = true;
-    sudo.u2fAuth = true;
+  config = lib.mkIf cfg.enable {
+    security.pam.services = {
+      login.u2fAuth = true;
+      sudo.u2fAuth = true;
+    };
+
+    # https://github.com/solokeys/solo2-cli/blob/main/70-solo2.rules
+    services.udev.packages = [
+      pkgs.yubikey-personalization
+      (pkgs.writeTextFile {
+        name = "wally_udev";
+        text = ''
+          # NXP LPC55 ROM bootloader (unmodified)
+          SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1fc9", ATTRS{idProduct}=="0021", TAG+="uaccess"
+          # NXP LPC55 ROM bootloader (with Solo 2 VID:PID)
+          SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1209", ATTRS{idProduct}=="b000", TAG+="uaccess"
+          # Solo 2
+          SUBSYSTEM=="tty", ATTRS{idVendor}=="1209", ATTRS{idProduct}=="beee", TAG+="uaccess"
+          # Solo 2
+          SUBSYSTEM=="usb", ATTRS{idVendor}=="1209", ATTRS{idProduct}=="beee", TAG+="uaccess"
+        '';
+        destination = "/etc/udev/rules.d/70-solo2.rules";
+      })
+    ];
   };
-
-  # https://github.com/solokeys/solo2-cli/blob/main/70-solo2.rules
-  services.udev.packages = [
-    pkgs.yubikey-personalization
-    (pkgs.writeTextFile {
-      name = "wally_udev";
-      text = ''
-        # NXP LPC55 ROM bootloader (unmodified)
-        SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1fc9", ATTRS{idProduct}=="0021", TAG+="uaccess"
-        # NXP LPC55 ROM bootloader (with Solo 2 VID:PID)
-        SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1209", ATTRS{idProduct}=="b000", TAG+="uaccess"
-        # Solo 2
-        SUBSYSTEM=="tty", ATTRS{idVendor}=="1209", ATTRS{idProduct}=="beee", TAG+="uaccess"
-        # Solo 2
-        SUBSYSTEM=="usb", ATTRS{idVendor}=="1209", ATTRS{idProduct}=="beee", TAG+="uaccess"
-      '';
-      destination = "/etc/udev/rules.d/70-solo2.rules";
-    })
-  ];
-
 }
