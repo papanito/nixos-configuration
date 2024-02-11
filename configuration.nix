d012dd08d79c47f36d493d469e971f1a80883185
papanito <papanito@wyssmann.com>
2024-02-11 11:32:42 +0100

    feat: move boot config to clawfinger

--- configuration.nix
+++ configuration.nix
@@ -9,7 +9,6 @@
     ./hosts/clawfinger # Include the results of the hardware scan.
 
     ./modules
-    ./boot.nix
     ./users.nix
   ];
 
@@ -36,23 +35,6 @@
     LC_TIME = "de_CH.utf8";
   };
 
-
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
