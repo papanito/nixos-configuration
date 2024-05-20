705d50f748d3322345d49fcb9861985bc0e48ccd
papanito <papanito@wyssmann.com>
2024-05-20 11:50:31 +0200

    feat: add ibm cloud cli

--- modules/cloud.nix
+++ modules/cloud.nix
@@ -15,6 +15,7 @@ in
       google-cloud-sdk
       hcloud # A command-line interface for Hetzner Cloud, a provider for cloud virtual private servers1
       python311Packages.hcloud # Library for the Hetzner Cloud API
+      ibmcloud-cli # Command line client for IBM Cloud
       terraform
       terragrunt # A thin wrapper for Terraform that supports locking for Terraform state and enforces best practices
       terraform-docs # A utility to generate documentation from Terraform modules in various output formats
