4c37fd506fe1f7572932dcc4b2d3c595d077d4ec
papanito <papanito@wyssmann.com>
2024-06-14 22:05:01 +0200

    feat: add more apps

--- hosts/clawfinger/default.nix
+++ hosts/clawfinger/default.nix
@@ -73,8 +73,10 @@
     evince
     firefox
     google-chrome
+    celeste # GUI file synchronization client that can sync with any cloud provider
     logseq # A local-first, non-linear, outliner notebook for organizing and sharing your personal knowledge base
     mkcert # A simple tool for making locally-trusted development certificates
+    rclone # Command line program to sync files and directories to and from major cloud storage
     rpi-imager
     signal-desktop
     slides # Terminal based presentation tool
