f66a68d47a4bd3a5b36eb9d8beb8b80a78c16b1a
papanito <papanito@wyssmann.com>
2024-06-16 11:36:03 +0200

    fix: disbale logseq due to EOL electron

--- hosts/clawfinger/default.nix
+++ hosts/clawfinger/default.nix
@@ -74,7 +74,8 @@
     firefox
     google-chrome
     celeste # GUI file synchronization client that can sync with any cloud provider
-    logseq # A local-first, non-linear, outliner notebook for organizing and sharing your personal knowledge base
+    #logseq # A local-first, non-linear, outliner notebook for organizing and sharing your personal knowledge base
+    # disabled due to EOL electron
     mkcert # A simple tool for making locally-trusted development certificates
     rclone # Command line program to sync files and directories to and from major cloud storage
     rpi-imager
@@ -86,7 +87,6 @@
     gnomeExtensions.tailscale-qs # Add Tailscale to GNOME quick settings
     topgrade # Upgrade all the things
     timeline #  Display and navigate information on a timeline
-    thunderbird # A full-featured e-mail client
     vim
     watchman # Watches files and takes action when they change
     wl-clipboard
