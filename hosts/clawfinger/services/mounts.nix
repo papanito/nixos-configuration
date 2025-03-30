3b7699a95ef6502e97d5febba88114f12ca443a4
papanito <papanito@wyssmann.com>
2025-03-30 15:51:33 +0200

    Add mount for envy

--- hosts/clawfinger/services/mounts.nix
+++ hosts/clawfinger/services/mounts.nix
@@ -1,15 +1,28 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  fileSystems."/mnt/yunohost" = {
-    device = "admin@yuno.home:/media";
+  # fileSystems."/mnt/yunohost" = {
+  #   device = "admin@yuno.home:/media";
+  #   fsType = "sshfs";
+  #   options = [
+  #     "noauto"
+  #     "x-systemd.automount"
+  #     "_netdev"
+  #     "reconnect"
+  #     "identityfile=/home/papanito/.ssh/id_ssh_admin@yunohost.ed25519"
+  #     "allow_other"
+  #   ];
+  # };
+
+  fileSystems."/mnt/envy" = {
+    device = "admin@10.0.0.11:/media";
     fsType = "sshfs";
     options = [
       "noauto"
       "x-systemd.automount"
       "_netdev"
       "reconnect"
-      "identityfile=/home/papanito/.ssh/id_rsa"
+      "identityfile=/home/papanito/.ssh/id_ssh_admin@envy.ed25519"
       "allow_other"
     ];
   };
