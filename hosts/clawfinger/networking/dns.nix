fd50ed3dbe8a5770f3222efc175fc5d2eb527cc5
papanito <papanito@wyssmann.com>
2025-06-01 16:05:37 +0200

    Add reverse proxy for local lan

--- hosts/clawfinger/networking/dns.nix
+++ hosts/clawfinger/networking/dns.nix
@@ -1,13 +1,33 @@
 { config, pkgs, ... }:
 
 {
-services.coredns.enable = true;
-services.coredns.config =
-  ''
-    local {
-      template IN A  {
-          answer "{{ .Name }} 0 IN A 127.0.0.1"
-      }
-    }
-  '';
+  # networking.firewall = {
+  #   allowedTCPPorts = [
+  #     443
+  #   ];
+  #   allowedUDPPorts = [
+  #     53
+  #   ];
+  # };
+
+  security.acme = {
+    acceptTerms = true;
+    defaults = {
+      email = "aedu@wyssmann.com";
+    };
+  };
+
+  services.nginx = {
+    enable = true;
+    virtualHosts = let
+      SSL = {
+        enableACME = true;
+        forceSSL = true;
+      }; in {
+        "webcheck.local" = (SSL // {
+          locations."/".proxyPass = "http://127.0.0.1:8888/";
+
+        });
+      };
+  };
 }
