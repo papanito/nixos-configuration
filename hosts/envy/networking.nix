a49b2489508de6dbbb6ec8cca3fdd44806e5e63e
papanito <papanito@wyssmann.com>
2025-06-07 18:31:17 +0200

    Add fixed ip for envy

--- hosts/envy/networking.nix
+++ hosts/envy/networking.nix
@@ -2,5 +2,12 @@
 {
   networking = {
     hostName = "envy"; # Define your hostname
+
+    interfaces = {
+      enp0s20f0u5.ipv4.addresses = [{
+        address = "10.0.0.11";
+        prefixLength = 16;
+      }];
+    };
   };
 }
