446e8fcbafb879767b2d8b049cc7124b0d5a317e
papanito <papanito@wyssmann.com>
2025-06-10 21:29:59 +0200

    Move renamed gnome and gdm

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -11,10 +11,10 @@ in
   config = lib.mkIf cfg.enable {
     services = {
       udev.packages = with pkgs; [ gnome-settings-daemon ];
+      displayManager.gdm.enable = true;
+      desktopManager.gnome.enable = true;
       xserver = {
         enable = true;
-        displayManager.gdm.enable = true;
-        desktopManager.gnome.enable = true;
         videoDrivers = [ "displaylink" ];
         # Configure keymap in X11
         xkb = {
