cbbf9dcb68a385e5c5d1bd2d5613c86a61a8bcb8
papanito <papanito@wyssmann.com>
2025-07-15 22:43:57 +0200

    Remove python packages

--- modules/development.nix
+++ modules/development.nix
@@ -2,15 +2,6 @@
 
 let
    cfg = config.development;
-   my-python-packages = ps: with ps; [
-    pandas
-    requests
-    pip # The PyPA recommended tool for installing Python packages
-    django
-    pillow # The friendly PIL fork (Python Imaging Library)
-    jupyter # A high-level dynamically-typed programming language
-    notebook # Web-based notebook environment for interactive computing
-  ];
 in
 {
   options.development = {
@@ -19,7 +10,6 @@ in
   };
   config = lib.mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
-      (pkgs.python3.withPackages my-python-packages)
       bfg-repo-cleaner # removes large or troublesome blobs in a git repository like git-filter-branch does, but faster
       cargo # Downloads your Rust project's dependencies and builds your project
       delta # Syntax-highlighting pager for git
@@ -30,9 +20,6 @@ in
       pre-commit-hook-ensure-sops # pre-commit hook to ensure that files that should be encrypted with sops are
       go
       nodejs
-      nodePackages.zx # A tool for writing better scripts.
-      nodePackages.snyk # snyk library and cli utility
-      # obsidian # A powerful knowledge base that works on top of a local folder of plain text Markdown files
       openjdk21
       python3
       pipenv # Python Development Workflow for Humans
