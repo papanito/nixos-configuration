f8c2797e85a11ecea9e8f3cb709aa2ee8fbd89ab
papanito <papanito@wyssmann.com>
2025-12-19 17:19:35 +0100

    clawfinger: Set kernel to 6.17
    Due to evdi compile issues

--- hosts/clawfinger/hardware.nix
+++ hosts/clawfinger/hardware.nix
@@ -31,7 +31,7 @@
 
   boot = {
     extraModulePackages = [ config.boot.kernelPackages.evdi ];
-    kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.linuxPackages_6_17;
     initrd = {
       # The set of kernel modules in the initial ramdisk used during the boot process.
       availableKernelModules = [
