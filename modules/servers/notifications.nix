13a90a02ab802c50ea45343dc29a9ace6676c23a
papanito <papanito@wyssmann.com>
2025-12-29 18:53:56 +0100

    Add server modules

--- modules/servers/notifications.nix
+++ modules/servers/notifications.nix
@@ -1,70 +1,28 @@
 { config, pkgs, ... }:
-
 {
+   sops = {
+      secrets.GOOGLE_CHAT_KEY = {
+        sopsFile = ./secrets.yaml;
+      };
+   
+      templates."googlechat.env".content = ''
+        GOOGLE_CHAT_KEY=${config.sops.placeholder.GOOGLE_CHAT_KEY}
+      '';
+   };
+
    systemd.services = {
       "systemd-googlechat-notifier@" = {
-         enable = false;
+         enable = true;
          description = "Send notifications to google chat endpoint";
          wantedBy = [ "default.target" ];
          after = [ "network.target" ];
          serviceConfig = {
             Type = "oneshot";
             User = "root";
+            EnvironmentFile = config.sops.templates."googlechat.env".path;
          };
-         scriptArgs = "-s %i -u '${config.sops.placeholder.GOOGLE_CHAT_KEY}'";
-         script = ''
-            #!/usr/bin/env bash
-            # Send notifications to a chat channel about systemd service status
-            SERVICE=restic-backups-b2
-            URL=
-
-            # Function: Print a help message.
-            usage() {
-               echo "Usage: $0 -s SERVICENAME -u URL -f" 1>&2
-            }
-
-            # Function: Exit with error.
-            exit_abnormal() {
-               usage
-               exit 1
-            }
-            if [ "$#" -lt "4" ]; then
-               exit_abnormal
-            fi
-
-            while getopts "u:s:f" option; do
-               case $option in
-                  s )
-                     SERVICE=$OPTARG
-                     echo $SERVICE
-                     ;;
-                  u )
-                     URL=$OPTARG
-                     ;;
-                  f )
-                     FULLSTATUS=true
-                     ;;
-                  \? )
-                     exit_abnormal
-                  ;;
-                  *)
-                     exit_abnormal
-                  ;;
-               esac
-            done
-
-            if [ -v FULLSTATUS ]; then
-               svc_status=$(systemctl status --full $SERVICE)
-            else
-               svc_status=$(systemctl status $SERVICE | grep Active)
-            fi
-            svc_status=$(sed "s:'::g" <<< "$svc_status")
-            hostname=`/run/current-system/sw/bin/hostname`
-
-            if [[ $URL =~ "https://chat.googleapis.com/v1/spaces" ]]; then
-               /run/current-system/sw/bin/curl --data "{'text':'*$SERVICE@$hostname*\n\`\`\`$svc_status\`\`\`'}" --header 'Content-Type: application/json; charset=UTF-8' --request POST $URL
-            fi
-         '';
+         scriptArgs = "-s %i -j";
+         script = builtins.readFile ../../scripts/google_notify.sh;
       };
    };
 }
--- modules/servers/restic.nix
+++ modules/servers/restic.nix
@@ -1,10 +1,33 @@
-{ config, pkgs, ... }:
-
+{ config, pkgs, name, ... }:
 {
   environment.systemPackages = with pkgs; [
     restic
     autorestic
   ];
+
+   sops = {
+    secrets.restic_password = {
+      sopsFile = ./secrets.yaml;
+    };
+    secrets.B2_ACCOUNT_ID = {
+      sopsFile = ./secrets.yaml;
+    };
+    secrets.B2_ACCOUNT_KEY = {
+      sopsFile = ./secrets.yaml;
+    };
+    templates."restic.env".content = ''
+      B2_ACCOUNT_ID=${config.sops.placeholder.B2_ACCOUNT_ID}
+      B2_ACCOUNT_KEY=${config.sops.placeholder.B2_ACCOUNT_KEY}
+    '';
+  };
+
+  # Inject the dependency into the generated service
+  systemd.services."restic-backups-b2" = {
+    unitConfig = {
+      OnSuccess = "systemd-googlechat-notifier@%N.service";
+      OnFailure = "systemd-googlechat-notifier@%N.service";
+    };
+  };
 
   services.restic.backups = {
     b2 = {
@@ -15,11 +38,12 @@
       passwordFile = "${config.sops.secrets.restic_password.path}";
       # what to backup.
       paths = [
+        "/home/papanito"
         "/var/lib/paperless/"
         "/var/lib/redis-paperless/"
       ];
       # the name of your repository.
-      repository = "b2:papanito-private-backup:/{config.networking.hostName}";
+      repository = "b2:papanito-private-backup:/${name}";
       timerConfig = {
         # backup every 1d
         OnCalendar = "daily";
@@ -42,95 +66,5 @@
         "--keep-yearly 10"
       ];
     };
-    local = {
-      initialize = true;
-      #
-      #user = "restic";
-      #
-      passwordFile = "${config.sops.secrets.restic_password.path}";
-      # what to backup.
-      paths = [
-        "/home/papanito"
-        "/var/lib/paperless/"
-        "/var/lib/redis-paperless/"
-      ];
-      # the name of your repository.
-      repository = "/home/backup/backup/clawfinger";
-      timerConfig = {
-        # backup every 1d
-        OnCalendar = "daily";
-        Persistent = true; 
-      };
-
-      exclude = [
-        "**/cs"
-        "*/.android*"
-        "*/.Android*"
-        "*/.ansible/collections"
-        "*/.Apache*"
-        "*/.BurpSuite"
-        "*/.cache"
-        "*/.cargo"
-        "*/.chrome"
-        "*/.dartServer"
-        "*/.donet"
-        "*/.dotnet"
-        "*/.eclipse"
-        "*/.gem"
-        "*/.local"
-        "*/.m2/repository"
-        "*/.mozilla"
-        "*/.nuget"
-        "*/.nvm"
-        "*/.steam"
-        "*/.thumbnails"
-        "*/.vscode*/extensions"
-        "*/.wine"
-        "*/*.iso"
-        "*/*.log"
-        "*/*cache"
-        "*/*Cache"
-        "*/*thumbs"
-        "*/bin/*"
-        "*/*cache*"
-        "*/*Cache*"
-        "*/Downloads"
-        "*/Dropbox"
-        "*/Games"
-        "*/gnome-shell"
-        "*/go"
-        "*/google-chrome*"
-        "*/gPodder"
-        "*/Insync"
-        "*/Music"
-        "*/node_modules"
-        "*/Public"
-        "*/share"
-        "*/snap"
-        "**/tmp"
-        "*/Trash"
-      ];
-
-      # keep 7 daily, 5 weekly, and 10 annual backups
-      pruneOpts = [
-        "--keep-daily 7"
-        "--keep-weekly 5"
-        "--keep-monthly 6"
-        "--keep-yearly 10"
-      ];
-    };
   };
-  # Instead of doing this, you may alternatively hijack the
-  # `awsS3Credentials` argument to pass along these environment
-  # vars.
-  #
-  # If you specified a user above, you need to change it to:
-  # systemd.services.user.restic-backups-myaccount = { ... }
-  #
-  # systemd.services.restic-backups-b2 = {
-  #   # environment = {
-  #   #   B2_ACCOUNT_ID = "my_account_id_abc123";
-  #   #   B2_ACCOUNT_KEY = "my_account_key_def456";
-  #   # };
-  # };
 }
