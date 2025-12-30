3b682e94cebbbc54e19ed26541b2f688d47a365d
papanito <papanito@wyssmann.com>
2025-12-30 12:23:35 +0100

    rpi: Disable root and nixos users

--- profiles/rpi/users.nix
+++ profiles/rpi/users.nix
@@ -1,4 +1,21 @@
 { lib, config, pkgs, ... }:
 {
   # see ../servers/users.nix
+  # Disable the default RPi user
+  users.users.nixos = {
+    extraGroups = lib.mkForce [ ];
+    isNormalUser = lib.mkForce false;
+    # This ensures the user is effectively disabled in /etc/passwd
+  };
+  
+  # Ensure your admin is the only one with wheel access
+  users.mutableUsers = false; # Prevents 'passwd' from working, forces declarative state
+
+  users.users.root = {
+    # Locking the account password
+    hashedPassword = "!"; 
+    
+    # Ensure no SSH keys are snuck in from the RPi base profiles
+    openssh.authorizedKeys.keys = lib.mkForce [ ];
+  };
 }
--- profiles/servers/users.nix
+++ profiles/servers/users.nix
@@ -1,7 +1,8 @@
-{ lib, config, pkgs, ... }:
+{ lib, config, pkgs, ...}:
 {
   # allow nix-copy to live system
   nix.settings.trusted-users = [ "admin" ];
+  services.getty.autologinUser = lib.mkForce null;
 
   users.users.admin = {
     isNormalUser = true;
@@ -11,13 +12,17 @@
       "video"
     ];
     initialHashedPassword = "$6$/f0v6HdHFSMjukeZ$0/QeGATuWXvhYcU60XPo6Vsqfb1hwIajKGsp90ZthLH8ionsXMFTBXyRXDA119.Ej0ldc35gT9ua0pePtwILI1";
-    hashedPasswordFile = config.sops.secrets.defaul:w_password.path;
+    hashedPasswordFile = config.sops.secrets.default_password.path;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOrOn3Kj/+ztMtQAaq4pVvXgTsIs1ZOqQDbsA+nJMuRM admin@envy from clawfinger"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFTCwPNpVjW6R9vqpKgNSWgGS5hZMZcHwexAMl7E/OI2 admin@envy from clawfinger"
+    ];
   };
 
-  users.users.root = {
-    initialHashedPassword = "$6$/f0v6HdHFSMjukeZ$0/QeGATuWXvhYcU60XPo6Vsqfb1hwIajKGsp90ZthLH8ionsXMFTBXyRXDA119.Ej0ldc35gT9ua0pePtwILI1";
-    hashedPasswordFile = config.sops.secrets.default_password.path;
-  };
+  #users.users.root = {
+  #  initialHashedPassword = "$6$/f0v6HdHFSMjukeZ$0/QeGATuWXvhYcU60XPo6Vsqfb1hwIajKGsp90ZthLH8ionsXMFTBXyRXDA119.Ej0ldc35gT9ua0pePtwILI1";
+  #  hashedPasswordFile = config.sops.secrets.default_password.path;
+  #};
 
   security.polkit.enable = true;
 
@@ -35,13 +40,8 @@
     };
   };
   
-  users.users.admin.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOrOn3Kj/+ztMtQAaq4pVvXgTsIs1ZOqQDbsA+nJMuRM admin@envy from clawfinger"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFTCwPNpVjW6R9vqpKgNSWgGS5hZMZcHwexAMl7E/OI2 admin@envy from clawfinger"
-  ];
-
-  users.users.root.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOrOn3Kj/+ztMtQAaq4pVvXgTsIs1ZOqQDbsA+nJMuRM admin@envy from clawfinger"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFTCwPNpVjW6R9vqpKgNSWgGS5hZMZcHwexAMl7E/OI2 admin@envy from clawfinger"
-  ];
+  #users.users.root.openssh.authorizedKeys.keys = [
+  #  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOrOn3Kj/+ztMtQAaq4pVvXgTsIs1ZOqQDbsA+nJMuRM admin@envy from clawfinger"
+  #  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFTCwPNpVjW6R9vqpKgNSWgGS5hZMZcHwexAMl7E/OI2 admin@envy from clawfinger"
+  #];
 }
