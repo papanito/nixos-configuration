1c23e856974b3fc29b06e9a94b0cb87abf285938
papanito <papanito@wyssmann.com>
2025-12-31 18:55:29 +0100

    profiles: Move user sops config

--- profiles/servers/users.nix
+++ profiles/servers/users.nix
@@ -1,8 +1,15 @@
-{ lib, config, pkgs, ...}:
+{ lib, config, pkgs, sops-nix, ...}:
 {
   # allow nix-copy to live system
   nix.settings.trusted-users = [ "nixos" ];
   services.getty.autologinUser = lib.mkForce null;
+  users.mutableUsers = false;
+  sops = {
+    secrets.default_password = {
+      sopsFile = ../../secrets/secrets.yaml;
+      neededForUsers = true; # Critical: decrypts before users are created
+    };
+  };
 
   users.users.nixos = {
     isNormalUser = true;
@@ -12,6 +19,8 @@
       "video"
     ];
     hashedPasswordFile = config.sops.secrets.default_password.path;
+    # Kill the empty string fallback
+    initialHashedPassword = lib.mkForce null;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOrOn3Kj/+ztMtQAaq4pVvXgTsIs1ZOqQDbsA+nJMuRM nixos@envy from clawfinger"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFTCwPNpVjW6R9vqpKgNSWgGS5hZMZcHwexAMl7E/OI2 nixos@envy from clawfinger"
