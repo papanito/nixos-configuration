ef891b08d83ef5ddc08709cc9323a10af7d12721
papanito <papanito@wyssmann.com>
2026-01-10 11:29:19 +0100

    clawfinger: Mount paperless consume from lenovo

--- hosts/clawfinger/services/mounts.nix
+++ hosts/clawfinger/services/mounts.nix
@@ -1,18 +1,19 @@
-{ config, lib, pkgs, modulesPath, ... }:
-
+{ config, lib, pkgs, modulesPath, hosts, ... }:
+let
+  # Extract values from your existing hosts definition
+  targetIp = hosts.envy.deployment.targetHost;
+in
 {
-  # fileSystems."/mnt/yunohost" = {
-  #   device = "admin@yuno.home:/media";
-  #   fsType = "sshfs";
-  #   options = [
-  #     "noauto"
-  #     "x-systemd.automount"
-  #     "_netdev"
-  #     "reconnect"
-  #     "identityfile=/home/papanito/.ssh/id_ssh_admin@yunohost.ed25519"
-  #     "allow_other"
-  #   ];
-  # };
+  fileSystems."/home/papanito/paperless-inbox" = {
+    device = "${targetIp}:/var/lib/paperless/consume";
+    fsType = "sshfs";
+    options = [ 
+      "allow_other"          # Allows your local user to see it
+      "_netdev"              # Tells NixOS to wait for network before mounting
+      "x-systemd.automount"  # Mounts it only when you try to access the folder
+      "identityfile=/home/papanito/.ssh/id_ssh_admin@envy.ed25519"
+    ];
+  };
 
   fileSystems."/mnt/envy" = {
     device = "admin@10.0.0.11:/media";
