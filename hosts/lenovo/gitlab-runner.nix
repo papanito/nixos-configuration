cf4f9c7034ad4a4524ba85f10b2ec31289e10d9c
papanito <papanito@wyssmann.com>
2026-01-10 17:20:34 +0100

    lenovo: Add gitlab-runner

--- hosts/lenovo/gitlab-runner.nix
+++ hosts/lenovo/gitlab-runner.nix
@@ -2,10 +2,32 @@
 
 {
    sops = {
-    secrets.GITLAB_RUNNER_TOKEN = {
+    secrets.gitlab_runner_token = {
+      sopsFile = ../../profiles/servers/secrets.yaml;
       # GitLab runner runs as the "gitlab-runner" user
+      #owner = "gitlab-runner";
+      key = "gitlab_runner_token";
       owner = "gitlab-runner";
+      # Ensure the file is NOT executable so the shell won't try to run it
+      mode = "0400";
     };
+
+    templates."gitlab-runner-env".content = ''
+      CI_SERVER_URL="https://gitlab.com";
+      REGISTRATION_TOKEN="${config.sops.placeholder.gitlab_runner_token}"
+    '';
+  };
+
+  users.users.gitlab-runner = {
+    isSystemUser = true;
+    group = "paperless";
+    description = "Service account for gitlab-runner";
+    # Prevents interactive login
+    shell = pkgs.shadow; 
+    # If the serv ftice needs a home directory for config/state
+    createHome = true;
+        
+    home = "/var/lib/gitlab-runner";
   };
 
   # Enable Docker (required for the Docker executor)
@@ -18,13 +40,14 @@
       # You can name this anything (e.g., "my-local-runner")
       local-builder = {
         # Get your token from GitLab: Settings > CI/CD > Runners > New project runner
-        authenticationTokenConfigFile = "/etc/gitlab-runner/auth-token";
+        #authenticationTokenConfigFile = "${config.sops.secrets.gitlab_runner_token.path}";
+        registrationConfigFile = config.sops.templates."gitlab-runner-env".path;
         
         executor = "docker";
         dockerImage = "golang:1.25-bookworm"; # Default image if not specified in YAML
         
         # Tags are crucial! Use this in your .gitlab-ci.yml
-        tagList = [ "local-nixos" ];
+        tagList = [ "homelab" ];
 
         # Optional: speed up builds by allowing the runner to use host's CPU cores
         registrationFlags = [
