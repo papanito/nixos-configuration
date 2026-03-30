cfcd7ceff3ccc67ae39bb44a34943544742e0dbd
papanito <papanito@wyssmann.com>
2026-03-30 12:01:58 +0200

    profiles: Remove onlyoffice from pc office profile

--- profiles/pc/office.nix
+++ profiles/pc/office.nix
@@ -1,17 +1,20 @@
-{ config, pkgs, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
 let
-   cfg = config.office;
+  cfg = config.office;
 in
 {
   options.office = {
-    enable
-      = lib.mkEnableOption "enable office module";
+    enable = lib.mkEnableOption "enable office module";
   };
   config = lib.mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
       libreoffice-fresh # Comprehensive, professional-quality productivity suite, a variant of openoffice
-      onlyoffice-desktopeditors
       pdftk # Command-line tool for working with PDFs
       pdfchain # A graphical user interface for the PDF Toolkit (PDFtk)
       pdfarranger # Merge or split pdf documents and rotate, crop and rearrange their pages using an interactive and intuitive graphical interface
