f78c8f735c6005972761a6568d20f5803812bd7d
papanito <papanito@wyssmann.com>
2026-06-30 18:32:46 +0200

    chore: Clawfinger hardware

--- hosts/clawfinger/hardware.nix
+++ hosts/clawfinger/hardware.nix
@@ -40,16 +40,16 @@
     #kernelPackages = pkgs.linuxPackages_6_12;
     kernelPackages = pkgs.linuxPackages_latest.extend (
       lfinal: lprev: {
-#        opensnitch-ebpf = lprev.opensnitch-ebpf.overrideAttrs (
-#          old:
-#          # Fixed in 1.7.3: https://github.com/evilsocket/opensnitch/pull/1554
-#          assert lib.versionOlder old.version "1.7.3";
-#          {
-#            preBuild = old.preBuild or "" + ''
-#              makeFlagsArray+=(EXTRA_FLAGS="-Wno-microsoft-anon-tag -fms-extensions")
-#            '';
-#          }
-#        );
+        #        opensnitch-ebpf = lprev.opensnitch-ebpf.overrideAttrs (
+        #          old:
+        #          # Fixed in 1.7.3: https://github.com/evilsocket/opensnitch/pull/1554
+        #          assert lib.versionOlder old.version "1.7.3";
+        #          {
+        #            preBuild = old.preBuild or "" + ''
+        #              makeFlagsArray+=(EXTRA_FLAGS="-Wno-microsoft-anon-tag -fms-extensions")
+        #            '';
+        #          }
+        #        );
       }
     );
     initrd = {
@@ -101,11 +101,14 @@
     kernel.sysctl = {
       "vm.swappiness" = 10;
     };
+    loader.systemd-boot.extraArgs = "nvme_core.default_ps_max_latency_us=0 pcie_aspm=off pcie_port_pm=off";
   };
 
-  services.hardware.bolt.enable = true;
-  services.upower.enable = true;
-  services.jitterentropy-rngd.enable = true; # Random number generator, which injects entropy to the kernel
+  services = {
+    hardware.bolt.enable = true;
+    upower.enable = true;
+    jitterentropy-rngd.enable = true; # Random number generator, which injects entropy to the kernel
+  };
   # required for wondershapper
   systemd.network.netdevs.ifb0 = {
     enable = true;
