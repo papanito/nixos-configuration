4765b1b611fa9aeb7a63a26a92ece7adc8418d79
papanito <papanito@wyssmann.com>
2026-07-17 14:16:05 +0200

    Replce libreoffice with alternatives

--- profiles/pc/office.nix
+++ profiles/pc/office.nix
@@ -1,4 +1,9 @@
-{ config, pkgs, lib, ...}:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 let
   cfg = config.modules.office;
 in
@@ -8,7 +13,9 @@ in
   };
   config = lib.mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
-      libreoffice-fresh # Comprehensive, professional-quality productivity suite, a variant of openoffice
+      #libreoffice-fresh # Comprehensive, professional-quality productivity suite, a variant of openoffice
+      collabora-desktop # Collaborative Office for desktop, based on LibreOffice technology
+      onlyoffice-desktopeditors # Office suite that combines text, spreadsheet and presentation editors allowing to create, view and edit local documents
       pdftk # Command-line tool for working with PDFs
       pdfchain # A graphical user interface for the PDF Toolkit (PDFtk)
       pdfarranger # Merge or split pdf documents and rotate, crop and rearrange their pages using an interactive and intuitive graphical interface
